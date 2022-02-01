09421a5d17a77f239e2c5d3154c523986d6e9f22
cindykandie <cindyjk6@gmail.com>
2022-02-01 23:32:01 +0300

    day 1

---
+++ Filip Challenge/main.js
@@ -0,0 +1,17 @@
+let radioClick = document.querySelector(".pri");
+let bodyBg = document.querySelector("body");
+let radioBtn= document.querySelector(".main");
+
+//add click event to radioClick
+//change background color of body
+//change background color of button
+
+
+function clickAction(){
+    radioClick.style.background = "white";
+    radioClick.style.left = "150px";
+    radioClick.style.borderColor = "black";
+    bodyBg.style.background = "black";
+    radioBtn.style.background = "black";
+    radioBtn.style.borderColor = "white";
+}
